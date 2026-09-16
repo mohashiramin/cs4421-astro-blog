f698ea5448620786734d13e2096246400464d74d
Muhammad Amin <2644599@student.ul.ie>
2026-09-16 11:01:16 +0100

    feat (blog): added author profiles to blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,22 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, reference } from 'astro:content';
 import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
+
+const authors = defineCollection({
+	loader: glob({ base: './src/content/authors', pattern: '**/*.md' }),
+	schema: ({ image }) =>
+		z.object({
+			name: z.string(),
+			bio: z.string(),
+			avatar: image(),
+			socialLinks: z.array(
+				z.object({
+					label: z.string(),
+					url: z.string().url(),
+				}),
+			),
+		}),
+});
 
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
@@ -14,7 +30,8 @@ const blog = defineCollection({
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: z.optional(image()),
+			author: reference('authors'),
 		}),
 });
 
-export const collections = { blog };
+export const collections = { authors, blog };
