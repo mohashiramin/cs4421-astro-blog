6d5f82b6a3ff6738fcd2555f823c1fcb4120d6c4
Muhammad Amin <26445999@studentmail.ul.ie>
2026-09-23 15:24:02 +0100

    fix(ci): update ci workflow path and test script name

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,17 @@
 import eslintPluginAstro from "eslint-plugin-astro"
 
 export default [
+  {
+    ignores: [
+      ".astro/**",
+      "**/*.d.ts",
+    ],
+  },
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
+    files: ["src/**/*.{js,astro}"],
     rules: {
       // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
